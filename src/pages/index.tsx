04e69e65c62ee5b374e709b2dc8ae80969dead2d
Eric Afes <eriicafes@gmail.com>
2022-09-15 18:18:18 +0100

    Cleanup app, setup tailwindcss and absolute imports

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next"
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-screen w-screenn flex items-center justify-center text-2xl font-bold">Hello Next</div>
+  )
+}
+
+export default Home
